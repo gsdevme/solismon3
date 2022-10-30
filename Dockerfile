2679be95f561fe1b5763a54c96cb2d6983f5eb88
Gavin Staniforth <gsdevme@users.noreply.github.com>
2022-10-30 11:11:33 +0000

    feat: migrate from config file to environment variables including python3.11

--- Dockerfile
+++ Dockerfile
@@ -6,9 +6,10 @@ LABEL MAINTAINER="Andrius Kozeniauskas"
 LABEL NAME=solismon3
 
 RUN mkdir /solismon3
-COPY *.py *.txt /solismon3/
-
 WORKDIR /solismon3
+
+COPY .env main.py requirements.txt /solismon3
+ADD config /solismon3/config
 
 RUN pip install --upgrade pip \
   && pip3 install -r requirements.txt
--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 # ver. 0.0.29
-import config.config as config
-import config.registers as registers
 import logging
-import paho.mqtt.client as mqtt
-from sys import exit
+import traceback
 from json import dumps
+from sys import exit
 from time import strptime, mktime, sleep
+
+import paho.mqtt.client as mqtt
+from environs import Env
 from prometheus_client import start_http_server
 from prometheus_client.core import GaugeMetricFamily, REGISTRY
 from pysolarmanv5.pysolarmanv5 import PySolarmanV5
 
+import config.registers as registers
+
 metrics_dict = {}
-debug = 0
+env = Env()
 
 
 def add_modified_metrics(custom_metrics_dict):
@@ -25,13 +28,16 @@ def add_modified_metrics(custom_metrics_dict):
         metrics_dict['battery_power_out_modified'] = 'Battery Power Out(modified)', 0
         metrics_dict['grid_to_battery_power_in_modified'] = 'Grid to Battery Power In(modified)', 0
     else:
-        metrics_dict['battery_power_modified'] = 'Battery Power(modified)', custom_metrics_dict['battery_power_2'] * -1 # negative
-        metrics_dict['battery_power_out_modified'] = 'Battery Power Out(modified)', custom_metrics_dict['battery_power_2']
+        metrics_dict['battery_power_modified'] = 'Battery Power(modified)', custom_metrics_dict[
+            'battery_power_2'] * -1  # negative
+        metrics_dict['battery_power_out_modified'] = 'Battery Power Out(modified)', custom_metrics_dict[
+            'battery_power_2']
         metrics_dict['battery_power_in_modified'] = 'Battery Power In(modified)', 0
         metrics_dict['grid_to_battery_power_in_modified'] = 'Grid to Battery Power In(modified)', 0
 
     if total_load < met_pwr and custom_metrics_dict['battery_power_2'] > 0:
-        metrics_dict['grid_to_battery_power_in_modified'] = 'Grid to Battery Power In(modified)', custom_metrics_dict['battery_power_2']
+        metrics_dict['grid_to_battery_power_in_modified'] = 'Grid to Battery Power In(modified)', custom_metrics_dict[
+            'battery_power_2']
 
     # Present meter modified metrics
     if met_pwr > 0:
@@ -47,7 +53,8 @@ def add_modified_metrics(custom_metrics_dict):
     metrics_dict['total_load_power_modified'] = 'Total Load Power(modified)', total_load
 
     if 0 < custom_metrics_dict['total_dc_input_power_2'] <= total_load:
-        metrics_dict['solar_to_house_power_modified'] = 'Solar To House Power(modified)', custom_metrics_dict['total_dc_input_power_2']
+        metrics_dict['solar_to_house_power_modified'] = 'Solar To House Power(modified)', custom_metrics_dict[
+            'total_dc_input_power_2']
     elif custom_metrics_dict['total_dc_input_power_2'] == 0:
         metrics_dict['solar_to_house_power_modified'] = 'Solar To House Power(modified)', 0
     elif custom_metrics_dict['total_dc_input_power_2'] > total_load:
@@ -56,7 +63,7 @@ def add_modified_metrics(custom_metrics_dict):
     logging.info('Added modified metrics')
 
 
-def scrape_solis(debug):
+def scrape_solis():
     custom_metrics_dict = {}
     global metrics_dict
     metrics_dict = {}
@@ -64,10 +71,11 @@ def scrape_solis(debug):
     try:
         logging.info('Connecting to Solis Modbus')
         modbus = PySolarmanV5(
-            config.INVERTER_IP, config.INVERTER_SERIAL, port=config.INVERTER_PORT, mb_slave_id=1, verbose=debug)
+            env.str("INVERTER_IP"), env.int("INVERTER_SERIAL"), port=env.int("INVERTER_PORT"),
+            mb_slave_id=1, verbose=env.bool("DEBUG"), socket_timeout=env.int("INVERTER_SOCKET_TIMEOUT"))
     except Exception as e:
-        logging.error(f'{repr(e)}. Exiting')
-        exit(1)
+        e.add_note('Connection to Solis modbus failed')
+        raise e
 
     logging.info('Scraping...')
 
@@ -86,13 +94,13 @@ def scrape_solis(debug):
                 logging.debug(regs)
             except Exception as e:
                 if c == 3:
-                    logging.error(f'Cannot read registers {reg} length{reg_len}. Tried {c} times. Exiting {repr(e)}')
-                    exit(1)
+                    e.add_note(f'Cannot read registers {reg} length{reg_len}. Tried {c} times. Exiting {repr(e)}')
+                    raise e
                 else:
                     c += 1
-                    logging.error(f'Cannot read registers {reg} length {reg_len} {repr(e)}')
-                    logging.error(f'Retry {c} in 3s')
-                    sleep(3)  # hold before retry
+                    logging.info(f'Cannot read registers {reg} length {reg_len} {repr(e)}')
+                    logging.info(f'Retry {c} in 5s')
+                    sleep(5)  # hold before retry
                     continue
             break
 
@@ -158,17 +166,18 @@ def scrape_solis(debug):
     logging.info(f'Ignored registers: {regs_ignored}')
 
     # Create modified metrics
-    if config.MODIFIED_METRICS:
+    if env.bool("MODIFIED_METRICS"):
         add_modified_metrics(custom_metrics_dict)
     logging.info('Scraped')
 
 
 def publish_mqtt():
     mqtt_dict = {}
-    try:
-        if not config.PROMETHEUS:
-            scrape_solis(debug)
 
+    if not env.bool("PROMETHEUS"):
+        scrape_solis()
+
+    try:
         # Resize dictionary and convert to JSON
         for metric, value in metrics_dict.items():
             mqtt_dict[metric] = value[1]
@@ -177,28 +186,28 @@ def publish_mqtt():
         def on_message(mqttc, userdata, msg):
             print(f"Message received [{msg.topic}]: {msg.payload}")
 
-
         mqttc = mqtt.Client()
-        if config.MQTT_USER != '':
-            mqttc.username_pw_set(config.MQTT_USER, config.MQTT_PASS)
-        mqttc.connect(config.MQTT_SERVER, config.MQTT_PORT, config.MQTT_KEEPALIVE)
-        mqttc.on_connect = logging.info(f'Connected to MQTT {config.MQTT_SERVER}:{config.MQTT_PORT}')
+        if env.str("MQTT_USER") != '':
+            mqttc.username_pw_set(env.str("MQTT_USER"), env.str("MQTT_PASS"))
+        mqttc.connect(env.str("MQTT_SERVER"), env.int("MQTT_PORT"), env.int("MQTT_KEEPALIVE"))
+        mqttc.on_connect = logging.info(
+            f'Connected to MQTT {env.str("MQTT_SERVER")}:{env.int("MQTT_PORT")}')
 
         logging.info('Publishing MQTT')
-        mqttc.publish(topic=config.MQTT_TOPIC, payload=mqtt_json)
+        mqttc.publish(topic=env.str("MQTT_TOPIC"), payload=mqtt_json)
 
         mqttc.disconnect()
 
     except Exception as e:
-        logging.error(f'Could not connect to MQTT {repr(e)}')
-
+        e.add_note('Could not connect to MWTT')
+        raise e
 
 class CustomCollector(object):
     def __init__(self):
         pass
 
     def collect(self):
-        scrape_solis(debug)
+        scrape_solis()
         publish_mqtt()
 
         for metric, value in metrics_dict.items():
@@ -206,31 +215,34 @@ class CustomCollector(object):
 
 
 if __name__ == '__main__':
+    env.read_env()
+
     try:
-        if config.DEBUG:
+        if env.bool("DEBUG"):
             logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.DEBUG,
                                 datefmt='%Y-%m-%d %H:%M:%S')
-            debug = 1
         else:
             logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO,
                                 datefmt='%Y-%m-%d %H:%M:%S')
-            debug = 0
-
         logging.info('Starting')
 
-        if config.PROMETHEUS:
-            logging.info(f'Starting Web Server for Prometheus on port: {config.PROMETHEUS_PORT}')
-            start_http_server(config.PROMETHEUS_PORT)
+        if env.bool("PROMETHEUS"):
+            logging.info(f'Starting Web Server for Prometheus on port: {env.str("PROMETHEUS_PORT")}')
+            start_http_server(int(env.str("PROMETHEUS_PORT")))
 
             REGISTRY.register(CustomCollector())
             while True:
-                sleep(config.CHECK_INTERVAL)
+                sleep(env.int("CHECK_INTERVAL"))
 
         else:
             while True:
                 publish_mqtt()
-                sleep(config.CHECK_INTERVAL)
+                sleep(env.int("CHECK_INTERVAL"))
 
     except Exception as e:
-        logging.error(f'Cannot start: {repr(e)}')
+        logging.error(repr(e))
+
+        if env.bool("DEBUG"):
+            print(traceback.format_exc())
+
         exit(1)
